c6c4667c1ef1f2e17bfd5ed364fd766678d95070
Wasym <46717121+wasymshykh@users.noreply.github.com>
2021-02-14 06:57:09 +0500

    dashboard and participant partial work

--- launchpad/login.php
+++ launchpad/login.php
@@ -8,6 +8,7 @@ if (isset($_POST) && !empty($_POST)) {
 
     $m = new Members($db);
 
+    $check_password = true;
     if ($_POST['email'] && is_string($_POST['email']) && !empty(normal_text($_POST['email']))) {
         $email = normal_text($_POST['email']);
         
@@ -20,6 +21,15 @@ if (isset($_POST) && !empty($_POST)) {
             $member = $m->get_one("member_email", $email);
             if (!$member['status']) {
                 array_push($errors, "Email doesn't exists");
+                $check_password = false;
+            } else if ($member['data']['member_account_status'] !== 'A') {
+                if ($member['data']['member_account_status'] === 'U') {
+                    array_push($errors, "Your account is unverified");
+                    $check_password = false;
+                } else {
+                    array_push($errors, "Your account has been banned");
+                    $check_password = false;
+                }
             } else {
                 $member = $member['data'];
             }
@@ -28,19 +38,21 @@ if (isset($_POST) && !empty($_POST)) {
         array_push($errors, "Email cannot be empty");
     }
 
-    if ($_POST['password'] && is_string($_POST['password']) && !empty(normal_text($_POST['password']))) {
-        $password = normal_text($_POST['password']);
-
-        if (empty($errors)) {
-            $password_length = 4;
-            if (strlen($password) < $password_length) {   
-                array_push($errors, "Password length must be minimum $password_length characters");
-            } else if (!password_verify($password, $member['member_password'])) {
-                array_push($errors, "Provided password is invalid");
+    if ($check_password) {
+        if ($_POST['password'] && is_string($_POST['password']) && !empty(normal_text($_POST['password']))) {
+            $password = normal_text($_POST['password']);
+    
+            if (empty($errors)) {
+                $password_length = 4;
+                if (strlen($password) < $password_length) {   
+                    array_push($errors, "Password length must be minimum $password_length characters");
+                } else if (!password_verify($password, $member['member_password'])) {
+                    array_push($errors, "Provided password is invalid");
+                }
             }
+        } else {
+            array_push($errors, "Password cannot be empty");
         }
-    } else {
-        array_push($errors, "Password cannot be empty");
     }
 
     if (empty($errors)) {
@@ -48,7 +60,7 @@ if (isset($_POST) && !empty($_POST)) {
         
         $_SESSION['member_logged'] = $member['member_id'];
         go (URL . '/launchpad/dashboard.php');
-        
+
     }
 
 }
@@ -57,10 +69,11 @@ if (isset($_POST) && !empty($_POST)) {
 ?>
 
 
-
-
 <?php if (isset($s_success) && !empty($s_success)): ?>
     <p style="color: green"><?=$s_success?></p>
+<?php endif; ?>
+<?php if (isset($s_error) && !empty($s_error)): ?>
+    <p style="color: red"><?=$s_error?></p>
 <?php endif; ?>
 
 <?php if (!empty($errors)): ?>
