37c7778717ed536bd3262424f88b756ce1f4e601
Wasym <46717121+wasymshykh@users.noreply.github.com>
2021-02-25 06:24:28 +0500

    code compilation

---
+++ code_compilation/index.php
@@ -0,0 +1,42 @@
+<?php
+
+    $unique_name = time();
+    $code_folder = "code/".$unique_name."/";
+    
+    if (!isset($_POST['code']) || empty(trim($_POST['code']))) {
+        die(json_encode(['status' => false, 'error' => 'E08.0']));
+    }
+    $code = $_POST['code'];
+
+    $mkdir = mkdir($code_folder);
+    $file = fopen($code_folder.'script.py', "a") or die(json_encode(['status' => false, 'error' => 'E08.1']));
+    fwrite($file, $code) or die(json_encode(['status' => false, 'error' => 'E08.2']));
+    fclose($file);
+
+    // creating Dockerfile
+    $file = fopen($code_folder.'Dockerfile', "a") or die(json_encode(['status' => false, 'error' => 'E08.3']));
+    $dockerFile = "FROM python:3.8-slim\nADD script.py /\nCMD [ \"python\", \"./script.py\" ]";
+    fwrite($file, $dockerFile) or die(json_encode(['status' => false, 'error' => 'E08.4']));
+
+    $command = "sudo docker build -t $unique_name --rm ".$code_folder;
+    exec($command, $o, $s);
+    if ($s) {
+        die(json_encode(['status' => false, 'error' => 'E08.5']));
+    }
+    
+    $command = "sudo docker run -i --name c_$unique_name --rm $unique_name 2>&1";
+    $ol = exec($command, $output, $status);
+    
+    // removing image
+    $command = "sudo docker rmi $unique_name";
+    exec($command, $o, $s);
+    // removing folder and files
+    unlink($code_folder.'script.py');
+    unlink($code_folder.'Dockerfile');
+    rmdir($code_folder);
+
+    if ($status) {
+        die(json_encode(['status' => true, 'type' => 'error', 'data' => $ol]));
+    }
+
+    die(json_encode(['status' => true, 'data' => $output]));
